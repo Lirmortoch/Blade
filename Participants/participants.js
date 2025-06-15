2dfaa4d52ae08ef5b213f7c48a733c60dfa406df
Lirmortoch <dima2111323@gmail.com>
2025-06-15 17:36:58 +0300

    Решили проблему с контейнором для слайдеров

--- Participants/participants.js
+++ Participants/participants.js
@@ -5,3 +5,10 @@ function setTopForStyles() {
 }
 
 setTopForStyles();
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // код для мобильных устройств
+} 
+else {
+    // код для обычных устройств
+}
