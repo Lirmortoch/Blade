51ddc41ddb8ec6198192c14d49c9941440787da7
Lirmortoch <dima2111323@gmail.com>
2025-06-29 15:01:02 +0300

    Добавил скрипт для пагинации

--- Participants/participants.js
+++ Participants/participants.js
@@ -13,7 +13,7 @@ setTopForStyles();
 //     // код для обычных устройств
 // }
 
-const participantItemCount = document.querySelectorAll('.participant-item').length;
+const participantItemCount = document.querySelectorAll('.carousel-item').length;
 
 function createPagnitation() {
     const paginationBlock = document.querySelector('.carousel__pagnitation');
@@ -21,9 +21,9 @@ function createPagnitation() {
 
     paginationBlock.innerHTML += paginationItem + paginationItem.replace('active', '').repeat(participantItemCount-1);
 }
-
 createPagnitation();
 
+const carouselWrap = document.querySelector('.carousel__wrap');
 let itemCount = 1;
 
 function mainSliderNextSlide(e) {
@@ -33,7 +33,6 @@ function mainSliderNextSlide(e) {
 
     if (itemCount < participantItemCount) itemCount++;
 
-    const carouselWrap = document.querySelector('.carousel__wrap');
     const slide = document.querySelector(`.carousel-item:nth-child(${itemCount})`);
     const leftButton = document.querySelector('.carousel__left-key');
 
@@ -65,7 +64,6 @@ function mainSliderPrevSlide(e) {
 
     if (itemCount > 0) itemCount--;
 
-    const carouselWrap = document.querySelector('.carousel__wrap');
     const slide = document.querySelector(`.carousel-item:nth-child(${itemCount})`);
     const rightButton = document.querySelector('.carousel__right-key');
 
@@ -93,6 +91,38 @@ function mainSliderPrevSlide(e) {
 
 document.addEventListener('click', mainSliderNextSlide);
 document.addEventListener('click', mainSliderPrevSlide);
+
+function swipeSliderToRight() {
+    if (itemCount < participantItemCount) itemCount++;
+
+    document.querySelector('.carousel-pagnitation__item.active').classList.remove('active');
+    document.querySelector(`.carousel-pagnitation__item:nth-child(${itemCount})`).classList.add('active');
+}
+function swipeSliderToLeft() {
+    if (itemCount > 0) itemCount--;
+
+    document.querySelector('.carousel-pagnitation__item.active').classList.remove('active');
+    document.querySelector(`.carousel-pagnitation__item:nth-child(${itemCount})`).classList.add('active');
+}
+
+let touchStartX = 0;
+let touchEndX = 0;
+
+function checkDirection() {
+    if (touchEndX < touchStartX) swipeSliderToRight();
+    if (touchEndX > touchStartX) swipeSliderToLeft();
+}
+
+function touchStartFnc(e) {
+    touchStartX = e.changedTouches[0].screenX;
+}
+function touchEndFnc(e) {
+    touchEndX = e.changedTouches[0].screenX;
+    checkDirection();
+}
+
+carouselWrap.addEventListener('touchstart', touchStartFnc);
+carouselWrap.addEventListener('touchend', touchEndFnc);
 
 function smallSliderNextSlide(e) {
     const closestElem = e.target.closest('.participant-carousel__right-key');
