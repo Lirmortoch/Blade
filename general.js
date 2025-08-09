cb6db2b4b4cbc19d06d9f4dcd756ec5e1c3177e5
Lirmortoch <dima2111323@gmail.com>
2025-08-09 21:12:27 +0300

    Полностью добил планшет вёрстку

--- general.js
+++ general.js
@@ -237,12 +237,12 @@ function showPopUpContent(e) {
     
     const popUp = document.querySelector('.pop-up');
     const title = popUp.querySelector('.pop-up__title');
-    const form = document.querySelector('.form');
+    const form = btn.parentElement.parentElement.querySelector('.form');
     const mainPartOfPopUp = popUp.querySelector('.pop-up__main-part');
 
     let elem, titleContent;
 
-    if (btnText === 'купить' && form?.checkValidity()) {
+    if (btnText === 'купить') {
         elem = 
             `
                 <form class='pop-up__form pop-up-form form tickets-form' action='#' onsubmit="return false" method="post">
@@ -287,6 +287,8 @@ function showPopUpContent(e) {
         titleContent = 'Чтобы приобрести билет, заполните форму ниже. Мы свяжемся с вами по вопросу оплаты.';
 
         popUp.classList.add('active');
+        title.clearHTMLElement().appendElement(titleContent);
+        mainPartOfPopUp.innerHTML = elem;
     }
     else if (/(бронь билета)|(отправить)/.test(btnText) && form?.checkValidity()) {
         elem = 
@@ -297,11 +299,16 @@ function showPopUpContent(e) {
                 <button class='pop-up__close-btn pop-up-button common-button-1 uppercase'>Понятно</button>
             `;
         
-        titleContent = '<svg width="13" height="10" viewBox="0 0 13 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12.5242 2.84518L10.8988 4.51251L6.0237 9.51152C5.73022 9.81387 5.32363 10 4.87512 10C4.42661 10 4.02003 9.81387 3.72557 9.51152L0.475812 6.17885C0.181355 5.87749 0 5.4609 0 5C0 4.07918 0.727368 3.33366 1.62439 3.33366C2.07388 3.33366 2.47949 3.5198 2.77394 3.82214L4.87512 5.97597L8.60069 2.15482L10.2261 0.488477C10.5195 0.187118 10.9261 0 11.3746 0C12.2726 0 13 0.746504 13 1.66634C13 2.12724 12.8186 2.54382 12.5242 2.84518Z" fill="currentColor"/></svg>';
+        titleContent = `
+            <svg width="13" height="10" viewBox="0 0 13 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12.5242 2.84518L10.8988 4.51251L6.0237 9.51152C5.73022 9.81387 5.32363 10 4.87512 10C4.42661 10 4.02003 9.81387 3.72557 9.51152L0.475812 6.17885C0.181355 5.87749 0 5.4609 0 5C0 4.07918 0.727368 3.33366 1.62439 3.33366C2.07388 3.33366 2.47949 3.5198 2.77394 3.82214L4.87512 5.97597L8.60069 2.15482L10.2261 0.488477C10.5195 0.187118 10.9261 0 11.3746 0C12.2726 0 13 0.746504 13 1.66634C13 2.12724 12.8186 2.54382 12.5242 2.84518Z" fill="currentColor"/></svg>
+        `;
 
         title.classList.add('title-check-mark');
         if (btnText === 'отправить') mainPartOfPopUp.classList.add('pop-up-okay');
+        
         popUp.classList.add('active');
+        title.clearHTMLElement().appendElement(titleContent);
+        mainPartOfPopUp.innerHTML = elem;
     }
     else if (btnText === 'задать вопрос') {
         elem = 
@@ -319,22 +326,19 @@ function showPopUpContent(e) {
 
                     <fieldset class="form__group">
                         <label for="input-msg" class="form__label">Сообщение</label>
-                        <input type="text" name="input-msg" id="input-msg" class="form__input" placeholder="Задайте любой вопрос" required>
+                        <textarea name="input-msg" id="input-msg" class="form__input" placeholder="Задайте любой вопрос" required></textarea>
                     </fieldset>
 
                     <button type="submit" class="form__submit-btn common-button-1 uppercase open-modal-btn open-modal-btn-text">Отправить</button>
-
-                    <!-- <button type="submit" class="form__submit-btn open-modal-btn open-modal-btn-text common-button-1 uppercase">Отправить</button> -->
                 </form>
             `;
 
         titleContent = 'Остались вопросы? Спросите нас! Мы ответим!';
 
         popUp.classList.add('active');
+        title.clearHTMLElement().appendElement(titleContent);
+        mainPartOfPopUp.innerHTML = elem;
     }
-
-    title.clearHTMLElement().appendElement(titleContent);
-    mainPartOfPopUp.innerHTML = elem;
     
     if (window.innerWidth < 1400) {
         document.body.classList.add('pop-up-lock');
